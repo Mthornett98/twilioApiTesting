f0bf8433ed6035642ee01c83b4a2db67cff6b34b
Matthew Thornett <matt.thornett1998@gmail.com>
2024-12-15 18:43:07 -0700

    Added twilio.env

--- explore.js
+++ explore.js
@@ -7,6 +7,10 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
+console.log('Account Sid:', process.env.TWILIO_ACCOUNT_SID);
+console.log('Auth Token:', process.env.TWILIO_AUTH_TOKEN);
+
+
 async function listMessage() {
   const messages = await client.messages.list({ limit: 20 });
 
